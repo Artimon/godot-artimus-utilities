80aa1f9cbb4c0e82509da32c0fb262f5294a8512
Artimon <webmaster@pad-soft.de>
2026-06-11 09:34:54 +0200

    Adds InfinityHub service worker manager.

---
+++ Services/Worker/IExplorer.cs
@@ -0,0 +1,11 @@
+﻿namespace Artimus.Services.Worker {
+	/**
+	 * A certain type of job that is sent through the corresponding
+	 * Stargate and to be expected to return with the result (callback).
+	 */
+	public interface IExplorer {
+		public void Process();
+
+		public void Finish() { }
+	}
+}
---
+++ Services/Worker/InfinityHub.cs
@@ -0,0 +1,28 @@
+﻿using Godot;
+
+namespace Artimus.Services.Worker;
+
+[GlobalClass]
+public partial class InfinityHub : Node {
+	public static InfinityHub instance;
+
+	public readonly Stargate _genericStargate = new ();
+
+	public override void _EnterTree() {
+		instance = this;
+	}
+
+	public override void _Process(double delta) {
+		_genericStargate.Finish();
+	}
+
+	public void EnterGenericGate(IExplorer explorer) {
+		_genericStargate.Enter(explorer);
+	}
+
+	public override void _ExitTree() {
+		_genericStargate.StopWorker();
+
+		instance = null;
+	}
+}
